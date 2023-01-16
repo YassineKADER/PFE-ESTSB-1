ddbb7952ede91bcb42c757f7fd3419168be1ea6a
YassineKADER <yassine.kader2017fc@gmail.com>
2023-01-16 14:37:36 +0100

    some icons added

--- static/js/forgot.js
+++ static/js/forgot.js
@@ -6,10 +6,10 @@ form.addEventListener('submit', (event) =>{
     data = {"email":email}
     fetch("/forgotpassword",{method:'POST',headers: {'Content-Type': 'application/json'}, body : JSON.stringify(data)}).then(response => response.json()).then(data =>{
         if(data.sent == true){
-            forgotsection.innerHTML = "<h3>Thank's for your interest, The mail has been sent to your email box</h3>"
+            forgotsection.innerHTML = "<h3>Thank's for your interest, The mail has been sent to your email box</h3><i class='fa-solid fa-envelope-circle-check' style='color:green;text-align:center;font-size:50px;'></i>"
         }
         else{
-            forgotsection.innerHTML = "<h3>An error hmm...., Try again later</h3>"
+            forgotsection.innerHTML = "<h3>An error hmm...., Try again later</h3><i class='fa-solid fa-triangle-exclamation' style='color:red;text-align:center;font-size:50px'></i>"
         }
     })
 })

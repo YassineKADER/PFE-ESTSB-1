e5c79d60560bee8cd1a91f77d7865fd8419f7ee8
EROS <80207770+YassineKADER@users.noreply.github.com>
2023-04-17 14:45:22 +0000

    Update Smart_Parking_System.py

--- Smart_Parking_System.py
+++ Smart_Parking_System.py
@@ -10,13 +10,13 @@ from threading import Thread
 app = Flask(__name__)
 
 config = {
-    "apiKey": "AIzaSyBgGx67w032_zncuZ37tFYPrm02rH1XbrY",
-    "authDomain": "wise-baton-353710.firebaseapp.com",
-    "databaseURL": "https://wise-baton-353710-default-rtdb.firebaseio.com",
-    "projectId": "wise-baton-353710",
-    "storageBucket": "wise-baton-353710.appspot.com",
-    "messagingSenderId": "962857669223",
-    "appId": "1:962857669223:web:3360987f13c2f1e6787ac2"
+    "apiKey": "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "authDomain": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "databaseURL": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "projectId": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "storageBucket": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "messagingSenderId": "xxxxxxxxxxxxxxxxxxxx",
+    "appId": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 }
 
 firebase = pyrebase.initialize_app(config)
